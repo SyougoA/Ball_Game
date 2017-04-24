e37e9a20c7d98a4423de91678d7cbb2885af99ac
今日も晴天♪ <kyoumoseiten@seitenbook-air.local>
2017-04-24 22:03:44 +0900

    add

---
+++ HelloUnity/Assets/Script/FallInChecker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallInChecker : MonoBehaviour {
+
+	public Hole red;
+	public Hole blue;
+	public Hole green;
+
+	public GUIStyleState styleState;
+	private GUIStyle style;
+
+	// Use this for initialization
+	void Start()
+	{
+		style = new GUIStyle();
+		style.fontSize = 50;
+		style.fontStyle = FontStyle.BoldAndItalic;
+	}
+
+	void OnGUI()
+	{
+
+
+		string label = " ";
+
+		if (red.IsFallIn() && blue.IsFallIn() && green.IsFallIn())
+		{
+			label = "Congratulations!";
+		}
+		Rect rect = new Rect(280, 100, 500, 500);
+
+		GUI.Label(rect, label, style);
+	}
+			
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ HelloUnity/Assets/Script/Reset.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour {
+
+	void OnGUI()
+	{
+		if (GUILayout.Button("Reload"))
+		{
+			SceneManager.LoadScene("BallGame");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
